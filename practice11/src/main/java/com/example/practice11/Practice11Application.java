274e29ab70a2359625a42faf49bb0754946b5741
german <7germania7@gmail.com>
2023-03-02 22:48:38 +0300

    pr11 spr boot+actuator rq

---
+++ practice11/src/main/java/com/example/practice11/Practice11Application.java
@@ -0,0 +1,13 @@
+package com.example.practice11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practice11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practice11Application.class, args);
+    }
+
+}
